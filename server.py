20366ecc256d3500498e725cb9b3836667357e46
mohamadalkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-06-06 21:57:21 +0300

    getting links

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
 import os
 import requests
-# from lxml.html import fromstring
 from HTMLParser import HTMLParser
+# import mysql.connector as mariadb
 
 def Main():
+		# mariadb_connection = mariadb.connect(user='python_user', password='some_pass', database='employees')
+		# cursor = mariadb_connection.cursor()
+
 		host = '127.0.0.1'
 		port = 5000
 
@@ -16,32 +19,14 @@ def Main():
 		print 'Connection from: ' + str(addr)
 		while True:
 				data = c.recv(4096)
-				# data = 'http://www.purplemath.com/modules/index.htm'
 				url = 'http://www.purplemath.com/modules/'
 				links = []
-				# if not data:
-				# 		break
 				r = requests.get(data)
-				# # print r.content.find('<h3 class="section-title" align="center">Intermediate Algebra')
-				# print r.content.find('<h3 class="section-title" align="center">Advanced Algebra Topics')
 				feed = r.content[32852:37575]
-				# print feed
-				
-				# create a subclass and override the handler methods
 				class MyHTMLParser(HTMLParser):
-
-				#     # def handle_starttag(self, tag, attrs):
-				#     # 	print 'starting tag :' + tag
-				#     def handle_data(self, data):
-				#     	if data == 'Intermediate Algebra Topics':
-				#     			print "Data     :", data
-				#     	if data == 'Advanced Algebra Topics':
-				#     			print "Data     :", data
-				#     			HTMLParser.close()
-				    
+  
 				    def handle_starttag(self, tag, attrs):
 				    	if tag == 'a':
-				    			# print "Encountered a start tag:", tag
 				    			for attr in attrs:
 				    				for att in attr:
 				    					if att == 'href':
